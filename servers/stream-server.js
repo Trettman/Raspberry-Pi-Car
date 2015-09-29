998331f4dc8976465d14b6dcbfe4fc67917a2207
Otto <mr.otto.1@hotmail.com>
2015-09-29 22:59:11 +0200

    Update

--- servers/stream-server.js
+++ servers/stream-server.js
@@ -12,23 +12,22 @@ if(process.argv.length < 3 ){
 var STREAM_SECRET = process.argv[2];
 var	STREAM_PORT = process.argv[3] || 8082;
 var	WEBSOCKET_PORT = process.argv[4] || 8084;
-var	STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
+var	STREAM_MAGIC_BYTES = "jsmp"; // Must be 4 bytes
 
-var webSocketServer = webSocket.Server({ port: WEBSOCKET_PORT });
+var webSocketServer = new webSocket.Server({ port: WEBSOCKET_PORT });
 
 var width = 320;
 var	height = 240;
 
 // Websocket Server
-//var socketServer = new (require("ws").Server)({port: WEBSOCKET_PORT});
-webSocketServer.on('connection', function(socket) {
+webSocketServer.on("connection", function(socket) {
 	// Send magic bytes and video size to the newly connected socket
 	// struct { char magic[4]; unsigned short width, height;}
 	var streamHeader = new Buffer(8);
 	streamHeader.write(STREAM_MAGIC_BYTES);
 	streamHeader.writeUInt16BE(width, 4);
 	streamHeader.writeUInt16BE(height, 6);
-	socket.send(streamHeader, {binary:true});
+	socket.send(streamHeader, { binary: true });
 
 	console.log("New WebSocket Connection(" + webSocketServer.clients.length + " total)");
 	
@@ -38,11 +37,10 @@ webSocketServer.on('connection', function(socket) {
 });
 
 webSocketServer.broadcast = function(data, opts) {
-	for( var i in this.clients ) {
-		if (this.clients[i].readyState == 1) {
+	for(var i in this.clients){
+		if(this.clients[i].readyState == 1){
 			this.clients[i].send(data, opts);
-		}
-		else {
+		} else{
 			console.log("Error: Client (" + i + ") not connected.");
 		}
 	}
@@ -53,7 +51,7 @@ webSocketServer.broadcast = function(data, opts) {
 var streamServer = http.createServer(function(request, response){
 	var params = request.url.substr(1).split("/");
 
-	if(params[0] == STREAM_SECRET) {
+	if(params[0] == STREAM_SECRET){
 		width = (params[1] || 320) | 0;
 		height = (params[2] || 240) | 0;
 		
@@ -73,5 +71,5 @@ var streamServer = http.createServer(function(request, response){
 	}
 }).listen(STREAM_PORT);
 
-console.log("Listening for MPEG Stream on http://127.0.0.1:" + STREAM_PORT + "/<secret>/<width>/<height>");
+console.log("Listening for MPEG Stream on http://127.0.0.1:" + STREAM_PORT + "/" + STREAM_SECRET + "/" + width + "/" + height);
 console.log("Awaiting WebSocket connections on ws://127.0.0.1:" + WEBSOCKET_PORT + "/");
